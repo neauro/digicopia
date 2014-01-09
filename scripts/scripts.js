52aa3e592ad08333d62c1603d02ad70e9e1e5e01
nadine <neauro@gmail.com>
2014-01-08 19:05:44 -0800

    animating to different parts of the page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,28 @@
 $(document).ready(function() {
   var menu = $("#mobile");
+  var scroll_speed = 1500;
+  var offset = -($("#nav").height()*4);
 
   $("#rwd-menu").on("click", function() {
     menu.toggleClass("open");
   });
+
+  $(".go-home").on("click", function() {
+    $('html, body').animate({
+      scrollTop: $("#container").offset().top-($("#nav").height()*4)
+    }, scroll_speed);
+  });
+
+  $(".go-work").on("click", function() {
+    $('html, body').animate({
+      scrollTop: $("#work").offset().top-($("#nav").height()*4)
+    }, scroll_speed);
+  });
+
+  $(".go-about").on("click", function() {
+    $('html, body').animate({
+      scrollTop: $("#like").offset().top-($("#nav").height())
+    }, scroll_speed);
+  });
+
 });
