b6ac4365aaef77fa63bc02521b10a9e546119aff
nadine <neauro@gmail.com>
2014-03-24 08:24:58 -0700

    making it so clicking 'anywhere' will close the mobile menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,18 @@
 var menu;
 $(document).ready(function() {
-  menu = $("#mobile");
+  menu = $(".mobile");
   var scroll_speed = 1500;
   var offset = -($("#nav").height()*4);
 
-  $(".close").on("click", function() {
-    console.log("DEBUG: close is clicked");
-    console.log(menu.attr("class"));
-    if ($(this).attr("id") != "rwd-menu") {
-      console.log("DEBUG: clicked thing is #rwd-menu");
-      if (menu.hasClass("open")) {
-        console.log("DEBUG: menu has open class");
-        menu.removeClass("open");
-      }
-      else {
-        console.log("DEBUG: menu does NOT have open class");
-      }
+  $(".mobile.overlay").on("click", function(e) {
+    e.stopPropagation(); 
+    console.log("DEBUG: clicked mobile overlay");
+    if (menu.hasClass("open")) {
+      console.log("DEBUG: menu has open class");
+      menu.removeClass("open");
     }
     else {
-      console.log("DEBUG: clicked thing is not #rwd-menu");
+      console.log("DEBUG: menu does NOT have open class");
     }
   });
 
